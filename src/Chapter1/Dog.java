94fe448015d135d3235c280b03c7e98634b492fa
Wang Ying <wy867504146@outlook.com>
2017-12-19 20:51:45 +0800

    My First Java App

---
+++ src/Chapter1/Dog.java
@@ -0,0 +1,18 @@
+package Chapter1;
+
+// Every Java application has to have at least one class,
+// and at least one main method (not one main per class;
+// just one main per application).
+public class Dog {
+    // The JVM runs everything between the curly braces
+    // {} of your main method.
+    public static void main(String[] args) {
+        // your code goes here
+    }
+
+    void bark() {
+        // statement1;
+        // statement2;
+    }
+
+}
