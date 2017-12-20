7c927291535394103433801636171162ddcf531c
Wang Ying <wy867504146@outlook.com>
2017-12-20 21:02:20 +0800

    Chapter2 practice codes

---
+++ src/Chapter2/DrumKit.java
@@ -0,0 +1,20 @@
+class DrumKit {
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playTopHat() {
+        System.out.println("ding ding da-ding");
+    }
+
+    void playSnare() {
+        System.out.println("bang bang ba-bang");
+    }
+}
+
+class DrumKitTestDrive {
+    public static void main(String[] args) {
+        DrumKit d = new DrumKit();
+        d.playSnare();
+        d.playTopHat();
+    }
+}
