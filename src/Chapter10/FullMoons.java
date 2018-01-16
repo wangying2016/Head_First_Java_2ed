71f1ef9c60beeead9152fe2ba6a6a64552f1a741
Wang Ying <wy867504146@outlook.com>
2018-01-16 11:17:12 +0800

    Chapter10 Test codes

---
+++ src/Chapter10/FullMoons.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import static java.lang.System.out;
+
+class FullMoons {
+    static int DAY_IM = 1000 * 60 * 60 * 24;
+
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        c.set(2004, 1, 7, 15, 50);
+        for (int x = 0; x < 60; x++) {
+            long day1 = c.getTimeInMillis();
+            day1 += (DAY_IM * 29.52);
+            c.setTimeInMillis(day1);
+            out.println(String.format("full moon on %tc", c));
+        }
+    }
+}
